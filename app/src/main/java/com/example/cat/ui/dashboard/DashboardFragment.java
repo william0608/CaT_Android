dee89e798ec13914b3713230d8826438ab12d2fc
william0608 <wnguimfack@gmail.com>
2021-06-01 21:41:42 +0200

    add response on click on

--- app/src/main/java/com/example/cat/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/cat/ui/dashboard/DashboardFragment.java
@@ -1,12 +1,15 @@
 package com.example.cat.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +17,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.cat.CompteRendu;
 import com.example.cat.R;
 import com.example.cat.databinding.FragmentDashboardBinding;
 
@@ -26,7 +30,7 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        String[] menuItems = {"Do something", "Do something else ", "Do yet another thing"};
+        String[] menuItems = {"La vie de la réunion, activités", "Comptes rendu des seances", "Liste des membres", "Situation Géographique"};
 
         ListView listView = (ListView) view.findViewById(R.id.mainMenu);
 
@@ -36,6 +40,21 @@ public class DashboardFragment extends Fragment {
                 menuItems
         );
         listView.setAdapter(listViewAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0){
+                    Toast.makeText(getActivity(), "tu as selection le premier", Toast.LENGTH_SHORT).show();
+                }else if (position == 1){
+                    Toast.makeText(getActivity(), "tu as selection le second", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getActivity(), CompteRendu.class);
+                    startActivity(intent);
+                } else if (position == 2){
+                    Toast.makeText(getActivity(), "tu as selection le troisieme", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         return view;
     }
     /*
